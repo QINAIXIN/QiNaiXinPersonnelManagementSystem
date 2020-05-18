1e119c87a1b4c9235c343d4db1479c6a47af00e9
QINAIXIN <44641632+QINAIXIN@users.noreply.github.com>
2020-05-18 20:40:49 +0800

    Update CorsConfig.java

--- vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/CorsConfig.java
+++ vhrserver/vhr-web/src/main/java/org/javaboy/vhr/config/CorsConfig.java
@@ -4,11 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * AJAX请求跨域
- * @author Mr.W
- * @time 2018-08-13
- */
+
 @Configuration
 public class CorsConfig extends WebMvcConfigurerAdapter {
     static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
